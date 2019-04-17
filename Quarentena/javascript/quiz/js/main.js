0b667893c4e876df0865fdf7dfe39b25687aa3c9
Alexandre Borges <augustusborges@gmail.com>
2019-04-17 19:36:51 -0300

    Curso Javascript

--- Quarentena/javascript/quiz/js/main.js
+++ Quarentena/javascript/quiz/js/main.js
@@ -1,1 +1,22 @@
 alert('Welcome to Quiz Ninja');
+
+
+const quiz = [
+	["What's Superman's name?", "Clark Kent"],
+	["What's Wonder Womam's real name?", "Diana Prince"],
+	["What is Batman's real name?", "Bruce Wayne"]
+];
+
+let score = 0;
+
+for(const [question,answer] of quiz){
+	
+	const response = prompt(question);
+	
+	if(response === answer){
+		alert('Correct!');
+		score++;
+	} else {
+		alert(`Wrong! The correct answer was ${answer}`);
+	}
+}
