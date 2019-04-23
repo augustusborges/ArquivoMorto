d11124e6397419de9a9fc15e7f78c6a4d0b7e3d5
Alexandre Borges <augustusborges@gmail.com>
2019-04-23 09:19:01 -0300

    Atualizando dados

---
+++ Quarentena/javascript/js/main.js
@@ -0,0 +1,57 @@
+//const btn = document.getElementById("btn");
+
+//const rainbow = ['red','orange','yellow','green','blue','rebeccapurple','violet'];
+
+//function change(){
+//	document.body.style.background = rainbow[Math.floor(7*Math.random())];
+//}
+
+//btn.addEventListener('click', change);
+
+
+		// const square = (x) => {resultado.innerHTML = x*x};
+
+		// function cubo(x){
+		// 	resultado.innerHTML = x*x*x;
+		// }
+
+		// const btnSquare = document.getElementById("btnSquare");
+		// const resultado = document.getElementById("resultado");
+
+		// btnSquare.addEventListener('click', function(){cubo(3)}, false);
+
+		// function triangulo(){
+		// 	var estrela = "*";
+		// 	for(var i=1; i<=7; i++){
+
+
+		// 		//do{
+
+		// 			console.log(estrela);
+		// 			estrela = estrela+"*";
+		// 		//}while()
+		
+		// 	}
+		// }
+
+		// triangulo();
+
+
+
+var day1 = {
+squirrel: false,
+events: ["work", "touched tree", "pizza", "running","television"]
+};
+console.log(day1.squirrel);
+// → false
+console.log(day1.wolf);
+// → undefined
+day1.wolf = true;
+console.log(day1.wolf);
+// → false
+
+console.log(day1.events[0]);
+
+let domm = document.getElementById("idspan");
+
+console.log(domm.innerHTML);
